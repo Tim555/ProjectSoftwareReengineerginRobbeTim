871bd13639e73a6acba748a7e2bd77c717f2296e
Robbe <php.robbe.claessens@gmail.com>
2019-05-15 22:12:11 +0200

    Pom

--- src/test/java/org/jfree/data/xy/DefaultXYZDatasetTest.java
+++ src/test/java/org/jfree/data/xy/DefaultXYZDatasetTest.java
@@ -230,4 +230,15 @@ public class DefaultXYZDatasetTest {
         return d;
     }
 
+    public void testZValue() {
+        DefaultXYZDataset d = new DefaultXYZDataset();
+        double[] x1 = new double[] {1.0, 2.0, 3.0};
+        double[] y1 = new double[] {4.0, 5.0, 6.0};
+        double[] z1 = new double[] {7.0, 8.0, 9.0};
+        double[][] data1 = new double[][] {x1, y1, z1};
+        d.addSeries("S1", data1);
+
+
+    }
+
 }
